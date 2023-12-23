9020f916d7d7678e9e3ebb1ad856bbd91a128357
GogolProjects <agagog97@gmail.com>
2023-12-23 08:05:37 +0100

    Fix

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -19,7 +19,7 @@ interface PageProps{
 }
 
 export const dynamic = 'force-dynamic'
-export const fetchCatche = 'force-no-sotre'
+export const fetchCache = 'force-no-sotre'
 
 const TravelitPostPage = async ({params}: PageProps) => {
 
