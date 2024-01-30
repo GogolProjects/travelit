3b5377ee98c73cbc66b6d3fd3807eaad473ff84b
GogolProjects <agagog97@gmail.com>
2024-01-30 14:15:16 +0100

    error fixing

--- src/components/CustomTravelit.tsx
+++ src/components/CustomTravelit.tsx
@@ -2,6 +2,7 @@ import { INFINITE_SCROLL_PAGINATION_RESULTS } from "../config"
 import { getAuthSession } from "../lib/auth"
 import { db } from "../lib/db"
 import GeneralTravelit from "./GeneralTravelit"
+
 import PostTravel from "./PostTravel"
 
 const CustomTravelit =async () => {
@@ -14,7 +15,7 @@ const CustomTravelit =async () => {
             subreddit: true,
         },
     })
-
+      
     const posts = await db.post.findMany({
         where: {
             subreddit: {
@@ -34,8 +35,10 @@ const CustomTravelit =async () => {
         },
         take: INFINITE_SCROLL_PAGINATION_RESULTS,
 
-    })
+    }) 
+       
+      
         return(<PostTravel initialPosts={posts}/>)
- }
-
+    }
+       
 export default CustomTravelit
